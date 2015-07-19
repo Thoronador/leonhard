09f91c9be566a295515228413e9eb06ce2b7e8c8
Dirk Stolle <striezel-dev@web.de>
2015-07-19 03:41:07 +0200

    Fibonacci: add parameter to change value of n via command line

--- fibonacci_cpp/main.cpp
+++ fibonacci_cpp/main.cpp
@@ -1,5 +1,6 @@
 /*
  -------------------------------------------------------------------------------
+    Program to compute a Fibonacci number via different methods.
     Copyright (C) 2015  Dirk Stolle
 
     This program is free software: you can redistribute it and/or modify
@@ -17,20 +18,80 @@
  -------------------------------------------------------------------------------
 */
 
+#include <algorithm>
 #include <chrono>
 #include <iostream>
+#include <sstream>
 #include "../libmath/fibonacci.hpp"
 
-int main()
+const int rcInvalidParameter = 1;
+
+int main(int argc, char **argv)
 {
   /* This program compares the computation times that are required to calculate
-     the 46th Fibonacci number via three different methods.
+     the 46th Fibonacci number via several different methods.
   */
+
+  bool doRecursive = false;
+  unsigned int custom_n = 0;
+
+  if (argc>1 and argv!=NULL)
+  {
+    int i=1;
+    while (i<argc)
+    {
+      if (argv[i]!=NULL)
+      {
+        const std::string param = std::string(argv[i]);
+        //recursive variant
+        if ((param=="-R") or (param=="--recursive"))
+        {
+          if (!doRecursive)
+          {
+            //enable slow recursive variant
+            doRecursive = true;
+          }
+          else
+          {
+            std::cout << "Error: \"" << param<<"\" must not be specified more than once.\n";
+            return rcInvalidParameter;
+          }
+        }//recursive option
+
+        else if (std::all_of(param.cbegin(), param.cend(), [](const char c) { return std::isdigit(c);} ))
+        {
+          if (custom_n > 0)
+          {
+            std::cout << "Error: You already have set the value of n to "
+                      << custom_n << ".\n";
+            return rcInvalidParameter;
+          }
+          std::istringstream stream(param);
+          stream >> custom_n;
+        }//specific value of n
+        else
+        {
+          //unknown or wrong parameter
+          std::cout << "Invalid parameter given: \"" << param << "\".\n"
+                    /*<< "Use --help to get a list of valid parameters.\n"*/;
+          return rcInvalidParameter;
+        }
+
+      }//parameter exists
+      else
+      {
+        std::cout << "Parameter at index " << i << " is NULL.\n";
+        return rcInvalidParameter;
+      }
+      ++i;//on to next parameter
+    }//while
+  }//if argc
+
   std::cout << "Computation time comparison for calculation methods of Fibonacci numbers:" << std::endl;
 
   libmath::DefaultFibonacci fibo;
 
-  const unsigned int n=45;
+  const unsigned int n = (custom_n > 0) ? custom_n : 45;
   //use iterative function
   const std::chrono::time_point<std::chrono::steady_clock> it_start = std::chrono::steady_clock::now();
   const uint64_t iterative = fibo.iterative(n);
@@ -40,14 +101,15 @@ int main()
             << " microsecond(s)" << std::endl;
 
   //recursive function (sloooooooooooow!)
-  const auto rec_start = std::chrono::steady_clock::now();
-  const uint64_t recursive = fibo.recursive(n);
-  const auto rec_end = std::chrono::steady_clock::now();
-  std::cout << "f("<<n<<") = " << recursive << " (recursive)" << std::endl
-            << "\tDuration: " << std::chrono::duration_cast<std::chrono::microseconds>(rec_end - rec_start).count() << " microseconds"
-            << " (ca. " << std::chrono::duration_cast<std::chrono::seconds>(rec_end - rec_start).count() << " seconds and "
-            << std::chrono::duration_cast<std::chrono::milliseconds>(rec_end - rec_start).count() % 1000 << " milliseconds)"
-            << std::endl;
+  if (doRecursive)
+  {
+    const auto rec_start = std::chrono::steady_clock::now();
+    const uint64_t recursive = fibo.recursive(n);
+    const auto rec_end = std::chrono::steady_clock::now();
+    std::cout << "f("<<n<<") = " << recursive << " (recursive)" << std::endl
+              << "\tDuration: " << std::chrono::duration_cast<std::chrono::microseconds>(rec_end - rec_start).count()
+              << " microseconds" << std::endl;
+  }
 
   //"fast" recursive function
   const auto rec_fast_start = std::chrono::steady_clock::now();
