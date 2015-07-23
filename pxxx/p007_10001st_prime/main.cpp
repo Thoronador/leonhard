8b5b11120b52710c70aea99b17cff17d9fa37403
Dirk Stolle <striezel-dev@web.de>
2015-07-24 00:58:55 +0200

    seventh program

---
+++ pxxx/p007_10001st_prime/main.cpp
@@ -0,0 +1,36 @@
+/*
+ -------------------------------------------------------------------------------
+    Copyright (C) 2015  Dirk Stolle
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ -------------------------------------------------------------------------------
+*/
+
+#include <cstdint>
+#include <iostream>
+#include <vector>
+#include "../../libmath/prime.hpp"
+
+int main()
+{
+  std::vector<uint_least64_t> primes;
+  uint_least64_t i = 0;
+  for (i=2; primes.size()<10001; ++i)
+  {
+    if (libmath::Prime<uint_least64_t>::is(i))
+      primes.push_back(i);
+  } //for
+  std::cout << "The 10001st prime is " << primes.back() << "." << std::endl;
+  return 0;
+}
