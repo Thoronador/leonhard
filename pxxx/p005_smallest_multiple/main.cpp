4dbf2ff9cc546b7645dff0cac67c0cdc216e8083
Dirk Stolle <striezel-dev@web.de>
2015-07-24 00:40:35 +0200

    license note for fifth program

--- pxxx/p005_smallest_multiple/main.cpp
+++ pxxx/p005_smallest_multiple/main.cpp
@@ -1,3 +1,22 @@
+/*
+ -------------------------------------------------------------------------------
+    Copyright (C) 2015  Dirk Stolle
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ -------------------------------------------------------------------------------
+*/
+
 #include <cstdint>
 #include <iostream>
 #include "../../libmath/prime.hpp"
@@ -13,9 +32,6 @@ void show(const std::vector<libmath::PrimeFactor<T> >& factors)
 
 int main()
 {
-  std::cout << "Hello world!" << std::endl;
-
-
   std::vector<libmath::PrimeFactor<uint_least64_t> > factors_current;
   std::vector<libmath::PrimeFactor<uint_least64_t> > factors_max = libmath::PrimeFactors<uint_least64_t>::compute(2);
 
